a8d7bedd32f516c97b3514e95dc544bfd2ec1816
Eduard-QA <eduard.mickel@gmail.com>
2024-10-18 14:24:29 +0300

    new lessons

--- src/Lessons/Lessons_14_Collections/Main.java
+++ src/Lessons/Lessons_14_Collections/Main.java
@@ -1,16 +1,25 @@
 package Lessons.Lessons_14_Collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        listMethods(); // создание коллекции и основные методы
+
+      listMethods(); // создание коллекции и основные методы
+
         List<String> filterA = new ArrayList<>(List.of("ass", "bss", "att", "ghh", "a34"));
+        System.out.println(filterA.iterator());
         System.out.println(filterA);
         System.out.println(filterStartWithA(filterA));
+
         mutable();    //new ArrayList<>(List.of("ww", "rr", "hh", "3")); => быстрое создание изменяемой коллекции
-//        immutable();  //выдаст ошибку потому что формат List.of создает неизменяемую коллекцию
+
+        Iterator<Integer> randomIterator = new RandomIterator(10);
+        random(randomIterator);
+
+        immutable();  //выдаст ошибку потому что формат List.of создает неизменяемую коллекцию
 
     }
 
@@ -57,4 +66,14 @@ public class Main {
         return result;
     }
 
+    public static void random(Iterator<Integer> number) {
+
+        StringBuilder result = new StringBuilder();
+        while (number.hasNext()) {
+            result.append(number.next()).append("; ");
+        }
+        System.out.println(result.toString());
+    }
 }
+
+
---
+++ src/Lessons/Lessons_14_Collections/RandomIterator.java
@@ -0,0 +1,26 @@
+package Lessons.Lessons_14_Collections;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class RandomIterator implements Iterator<Integer> {
+    Random random = new Random();
+    private final int limit;
+    private int counter;
+
+    public RandomIterator(int limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return counter<limit;
+    }
+
+    @Override
+    public Integer next() {
+        counter++;
+        return random.nextInt();
+    }
+
+}
